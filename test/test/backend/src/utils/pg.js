5ef48b394a70025893276cff81d12169fcc8aa36
Stephen <stephenopara405@gmail.com>
2025-05-02 14:01:12 +0100

    update pg.js

--- test/test/backend/src/utils/pg.js
+++ test/test/backend/src/utils/pg.js
@@ -3,10 +3,10 @@ const {Pool} = require('pg');
 const { database, password } = require('pg/lib/defaults.js');
 
 const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'postgres',
-    password:'imustgotoharvard11',
+    user:'me',
+    host:'dpg-d0ac2o95pdvs73ckko10-a',
+    database:'test2_rgnl',
+    password:process.env.DATABASE_PASSWORD,
     port:'5432'
 });
 
